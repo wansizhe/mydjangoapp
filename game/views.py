f8655c7f16c9f9af4edb98df451465530d72aec2
sizhe <sizhe.wan@gmail.com>
2022-04-05 20:31:56 +0800

    add first pages

--- game/views.py
+++ game/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align: center">Home Page</h1>'
+    line2 = '<a href="/play/">play game</a>'
+    return HttpResponse(line1 + line2)
+
+def play(request):
+    line1 = '<h1 style="text-align: center">Play Game</h1>'
+    line2 = '<a href="/">home</a>'
+    return HttpResponse(line1 + line2)
