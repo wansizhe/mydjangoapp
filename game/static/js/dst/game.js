86d4f5be2ee7515cccad9f6a511dcb8e85085802
sizhe <sizhe.wan@gmail.com>
2022-04-24 23:50:36 +0800

    create game map

--- game/static/js/dst/game.js
+++ game/static/js/dst/game.js
@@ -52,13 +52,88 @@ class AcGameMenu {
 		this.$menu.hide();
 	}
 }
-class AcGamePlayground {
+let AC_GAME_OBJECTS = [];
+
+class AcGameObject {
+    constructor() {
+        AC_GAME_OBJECTS.push(this);
+        this.has_called_start = false;
+        this.timedelta = 0;
+    }
+
+    start() {
+
+    }
+
+    update() {
+
+    }
+
+    on_destroy() {
+
+    }
+
+    destroy() {
+        this.on_destroy();
+        for (let i = 0; i < AC_GAME_OBJECTS.length; i++) {
+            if (AC_GAME_OBJECTS[i] === this) {
+                AC_GAME_OBJECTS.splice(i, 1);
+                break;
+            }
+        }
+    }
+}
+
+let last_timestamp;
+let AC_GAME_ANIMATION = function(timestamp) {
+    for (let i = 0; i < AC_GAME_OBJECTS.length; i++) {
+        let obj = AC_GAME_OBJECTS[i];
+        if (!obj.has_called_start) {
+            obj.start();
+            obj.has_called_start = true;
+        } else {
+            obj.timedelta = timestamp - last_timestamp;
+            obj.update();
+        }
+    }
+    last_timestamp = timestamp;
+
+    requestAnimationFrame(AC_GAME_ANIMATION);
+}
+
+requestAnimationFrame(AC_GAME_ANIMATION);class GameMap extends AcGameObject {
+    constructor(playground) {
+        super();
+        this.playground = playground;
+        this.$canvas = $(`<canvas></canvas>`);
+        this.ctx = this.$canvas[0].getContext('2d');
+        this.ctx.canvas.width = this.playground.width;
+        this.ctx.canvas.height = this.playground.height;
+        this.playground.$playground.append(this.$canvas);
+    }
+
+    start() {
+        
+    }
+
+    update() {
+        this.render();
+    }
+
+    render() {
+        this.ctx.fillStyle = "rgba(0, 0, 0)";
+        this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    }
+}class AcGamePlayground {
     constructor(root) {
         this.root = root;
-        this.$playground = $(`<div>游戏界面</div>`);
+        this.$playground = $(`<div class="ac-game-playground"></div>`);
 
-        this.hide();
+        // this.hide();
         this.root.$ac_game.append(this.$playground);
+        this.width = this.$playground.width();
+        this.height = this.$playground.height();
+        this.game_map = new GameMap(this);
 
         this.start();
     }
@@ -75,11 +150,11 @@ class AcGamePlayground {
         this.$playground.hide();
     }
 }
-class AcGame {
+export class AcGame {
     constructor(id) {
         this.id = id;
         this.$ac_game = $('#' + id);
-        this.menu = new AcGameMenu(this);
+        // this.menu = new AcGameMenu(this);
         this.playground = new AcGamePlayground(this);
         
         this.start();
