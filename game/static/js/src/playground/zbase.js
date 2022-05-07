5880ef56bd1b5bab45a717c287f243ed854b827f
sizhe <sizhe.wan@gmail.com>
2022-05-07 23:35:31 +0800

    turn off listening function after closing the windows in acapp

--- game/static/js/src/playground/zbase.js
+++ game/static/js/src/playground/zbase.js
@@ -14,11 +14,27 @@ class AcGamePlayground {
         return colors[Math.floor(Math.random() * colors.length)];
     }
 
+    create_uuid() {
+        let res = "";
+        for (let i = 0; i < 8; i++) {
+            let x = parseInt(Math.floor(Math.random() * 10));
+            res += x;
+        }
+        return res;
+    }
+
     start() {
         let outer = this;
-        $(window).resize(function() {
+        let uuid = this.create_uuid();
+        $(window).on(`resize.${uuid}`, function() {
             outer.resize();
         });
+
+        if (this.root.AcWingOS) {
+            this.root.AcWingOS.api.window.on_close(function() {
+                $(window).off(`resize.${uuid}`);
+            });
+        }
     }
 
     resize() {
